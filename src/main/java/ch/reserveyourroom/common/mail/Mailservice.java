3777d8d2bbfbf5c7512b0dcad455bed04b91b118
Philippe Wanner <philippe.wanner@tomylab.com>
2016-10-04 19:53:21 +0200

    last state

---
+++ src/main/java/ch/reserveyourroom/common/mail/Mailservice.java
@@ -0,0 +1,45 @@
+package ch.reserveyourroom.common.mail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+/**
+ * Created by Danijel
+ */
+public class Mailservice {
+
+    //private static final String SMTP_HOST = "smtp.bfh.ch";
+    private static final String SMTP_HOST = "mail.gmx.net";
+    private static final int SMTP_PORT = 587;
+
+    private static final String USERNAME = "d.brei@gmx.ch";
+    private static final String PASSWORD = "";
+
+    public static void sendMail(String recipient,
+                                String subject,
+                                String msg) throws EmailException
+    {
+        System.out.println("Entering sendMail-method");
+        SimpleEmail email = new SimpleEmail();
+        //HtmlEmail email = new HtmlEmail();
+        email.setDebug(true);
+
+        email.setHostName(SMTP_HOST);
+        //email.setSslSmtpPort("587");
+        email.setSmtpPort(465);
+
+        email.setAuthenticator(new DefaultAuthenticator(USERNAME, PASSWORD));
+        email.setSSLOnConnect(true);
+        //email.setStartTLSEnabled(true);
+
+        email.setFrom(USERNAME, "RYR Responder");
+        email.setSubject(subject);
+        email.setMsg(msg);
+        email.addTo(recipient);
+
+        email.send();
+        System.out.println("mail send to "+recipient);
+    }
+
+}
